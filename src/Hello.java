d5d940afaae16dd8f3fd184a734473f2ec177d8a
jukangpark <skyxxx9339@naver.com>
2022-07-23 11:01:35 +0900

    Tv 클래스 추가

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,30 @@
 public class Hello {
     public static void main(String[] args) {
-        int age = 25;
-        System.out.println(age);
+        byte b = 127;
+        b ++; // 정수의 overflow
+
+        int i = (char)b;
+
+        int[] score; // 배열을 선언한다.
+        score = new int[5]; // 배열을 생성한다. (5개의 int 값을 저장할 수 있는 공간 생성)
+
+        score[0] = 100;
+        score[1] = 90;
+        score[2] = 80;
+        score[3] = 70;
+        score[4] = 60;
+
+        System.out.println(score[1]);
+
+
+        System.out.println(i);
+        System.out.println(b);
         System.out.println("Hello World!"); // 화면에 글자 출력
+
+        Tv t;
+        t = new Tv();
+        t.channel = 7;
+        t.channelDown();
+        System.out.println(t.channel);
     }
 }
---
+++ src/Tv.java
@@ -0,0 +1,12 @@
+public class Tv {
+    String color;
+    boolean power;
+    int channel;
+    void power () { power = !power;} // 전원 on / off
+    void channelUp() { channel++; }
+    void channelDown() { channel--; }
+
+
+
+}
+
