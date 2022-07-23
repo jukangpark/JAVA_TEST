77ffb18e5d84ee40ead444095ecb94dd5cdebe3d
jukangpark <skyxxx9339@naver.com>
2022-07-23 10:18:05 +0900

    init

---
+++ src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        int age = 25;
+        System.out.println(age);
+        System.out.println("Hello World!"); // 화면에 글자 출력
+    }
+}
